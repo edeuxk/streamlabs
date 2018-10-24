704f8012a55eb4e00157a3b8bd6d5e141cb7dd79
Eddy Almeida <eddyalmeidapro@gmail.com>
2018-10-24 14:36:04 +0200

    Remove stats header

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,10 @@
 							<b-nav-item to="/">
 								Home
 							</b-nav-item>
-							<b-nav-item to="/stats">
+<!-- 							<b-nav-item to="/stats">
 								Stats
 							</b-nav-item>
-						</b-navbar-nav>
+ -->						</b-navbar-nav>
 
 						<b-navbar-nav class="ml-auto">
 							<b-nav-form>
